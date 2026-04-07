c0b8a54b38d202b5292e3c3d45aa3d3b247daeee
MaxKnyazev <max.knyazev77@gmail.com>
2026-04-07 14:31:23 +0300

    изменил дашборд

--- src/pages/admin/AdminDashboardPage.tsx
+++ src/pages/admin/AdminDashboardPage.tsx
@@ -1,19 +1,8 @@
-import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { StatsOverview } from '../../widgets/statistics/StatsOverview';
 
 export const AdminDashboardPage = () => (
   <Stack spacing={2}>
-    <Typography variant="h4">Админ дашборд</Typography>
-    <StatsOverview />
-    <Paper sx={{ p: 2 }}>
-      <Typography variant="h6">Распределение по статусам</Typography>
-      <Typography color="text.secondary">Ожидает саппорта: 40% | В работе: 30% | Закрыт: 30%</Typography>
-    </Paper>
-    <Paper sx={{ p: 2 }}>
-      <Typography variant="h6">Динамика нагрузки</Typography>
-      <Typography color="text.secondary">Графики интегрируются на следующем этапе, сейчас заглушка UI.</Typography>
-    </Paper>
+    <Typography variant="h4">дашборды будут в будущем</Typography>
   </Stack>
 );
